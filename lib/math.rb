cfc8765f458d753a1fbf70c02e8e6e65b68a0fcf
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-15 18:54:43 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  addition=num1+num2
+  addition
 end
 
 def subtraction(num1, num2)
-
+  subtraction=num1-num2
+  subtraction
 end
 
 def division(num1, num2)
-
+  division=num1/num2
 end
 
 def multiplication(num1, num2)
-
+  multiplication=num1*num2
+  multiplication
 end
 
 def modulo(num1, num2)
-
+  modulo=num1%num2
+  modulo
 end
 
 def square_root(num)
-
+  square_root=Math.sqrt(num)
+  square_root
 end
